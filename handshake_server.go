be80069b8aa6ed29aa2e535abfb9375ed71e8294
Mike Beaumont <mjboamail@gmail.com>
2024-04-29 12:14:32 +0200

    crypto/tls: don't call tlsrsakex.IncNonDefault with FIPS
    We haven't called tlsrsakex.Value() yet at this point if we're using
    FIPS, like if CipherSuites != nil. This adds needFIPS as a gate next to
    CipherSuites != nil. FIPS specifies suites that would be skipped if
    tlsarsakex were set.
    
    For #65991.
    Fixes #65994.
    
    Change-Id: I8070d8f43f27c04067490af8cc7ec5e787f2b9bd
    Reviewed-on: https://go-review.googlesource.com/c/go/+/582315
    Reviewed-by: Filippo Valsorda <filippo@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Bypass: Filippo Valsorda <filippo@golang.org>
    Auto-Submit: Filippo Valsorda <filippo@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    (cherry picked from commit 78e50d0fa0bc78d197bd1b41e1bdef8c20a03396)
    Reviewed-on: https://go-review.googlesource.com/c/go/+/593395
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Roland Shoemaker <roland@golang.org>

--- handshake_server.go
+++ handshake_server.go
@@ -374,7 +374,7 @@ func (hs *serverHandshakeState) pickCipherSuite() error {
 	}
 	c.cipherSuite = hs.suite.id
 
-	if c.config.CipherSuites == nil && rsaKexCiphers[hs.suite.id] {
+	if c.config.CipherSuites == nil && !needFIPS() && rsaKexCiphers[hs.suite.id] {
 		tlsrsakex.IncNonDefault()
 	}
 
